41ecd1ad3477936d8a3b456e27d7f0f96bd3c95f
sujayBTC <Sujay.S@bigthinkcode.com>
2025-11-26 12:33:40 +0530

    using alembic in our project

--- db.py
+++ db.py
@@ -5,6 +5,7 @@ import os
 
 # DB_URL = os.getenv("DB_URL")
 DB_URL = "postgresql+psycopg2://postgres:postgres@localhost:5432/practice"
+
 engine = create_engine(DB_URL,pool_pre_ping=True)
 session = sessionmaker(autocommit=False,autoflush=False,bind=engine)
 
